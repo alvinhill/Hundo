a5fdfc20d2d8c8bb30fb90e32ae238f06bb32ebc
Alvin Hill <132315899+alvinhill@users.noreply.github.com>
2025-03-23 13:17:19 -0700

    VERSION ONE COMMIT
    It works pretty well but the navbar on the first screen won't work.

--- js/site.js
+++ js/site.js
@@ -1,5 +1,60 @@
 
-// call hello world
-function HelloWorld(){
-    alert("hello world");
+// get values from page
+// start or controller function
+function getValues(){
+// get values from page
+let startValue = document.getElementById("startValue").value;
+let endValue = document.getElementById("endValue").value;
+
+// parse to integers
+startValue = parseInt(startValue);
+endValue =parseInt(endValue);
+// Validate input
+if (Number.isInteger(startValue) && Number.isInteger(endValue) ){
+// call generate Numbers
+  let numbers = genrateNumbers(startValue,endValue);
+// call display Numbers
+displayNumbers(numbers);
+
+}else{
+    alert("you must enter an integer")
 }
+
+}
+// generate sequential numbers from start to end values
+// logic function
+function genrateNumbers(startValue, endValue){
+let numbers = [];
+
+// want to get all numbers from start to end USE LOOP
+for(let index = startValue; index <= endValue; index++){
+    // loop code till end value
+    numbers.push(index);
+}
+return numbers;
+
+}
+// dispaly numbers
+// mark even numbers bold
+function displayNumbers(numbers){
+    let templateRows="";
+
+for (let index = 0; index < numbers.length; index++) {
+
+    let className= "even";
+    let number = numbers[index];
+    if(number % 2 == 0){
+        className="even";
+    }
+    else{
+className="odd";
+    }
+
+    templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+}
+
+document.getElementById("results").innerHTML = templateRows;
+
+
+}
+// display or view function
